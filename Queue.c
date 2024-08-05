bcb69f424bdd23db330d624647b45db18bdffb0c
Allen Jude <ajallen142001@gmail.com>
2024-08-05 23:09:35 +0530

    Initial commit

---
+++ Queue.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int t=-1;
+int n=10;
+int q[10];
+
+void enqueue(){}
+
+void dequeue(){}
+
+void display(){}
+
+int main() {
+ int c=1;
+ while(c!=4){
+  printf("Enter '1' for Enqueue \n");
+  printf("Enter '2' for Dequeue\n");
+  printf("Enter '3' for Display \n");
+  printf("Enter '4' to Exit \n");
+  printf("Enter ur choice :");
+  scanf("%d", &c);
+ 
+  if(c == 1){
+
+  }
+  
+  if(c == 2){
+
+  } 
+  
+  if(c == 3){
+  }
+ }
+}
